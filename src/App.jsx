0140f9319b2ce9320337c0407df6442613553844
m4rcin7 <mrku1337@gmail.com>
2025-06-16 20:20:39 +0200

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { Routes } from "@solidjs/router";
 import RoutesList from "./routes";
 
 export default function App() {
   return (
     <main style="padding: 1rem;">
       <h1>🧠 Mind Garden</h1>
-      <Routes>
-        <RoutesList />
-      </Routes>
+      <RoutesList />
     </main>
   );
 }
